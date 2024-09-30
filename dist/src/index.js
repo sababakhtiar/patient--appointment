9a606f4704e34de86e8fc73ba10e45a9805b02f5
sababakhtiar <sababakhtiar23@gmail.com>
2024-09-30 18:44:40 +0500

    update code

--- dist/src/index.js
+++ dist/src/index.js
@@ -3,6 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 require("reflect-metadata");
 const node_http_1 = require("node:http");
 const graphql_yoga_1 = require("graphql-yoga");
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-
+import dotenv from "dotenv";
+dotenv.config();
 import "reflect-metadata";
 import { createServer } from "node:http";
 import { createYoga } from "graphql-yoga";
